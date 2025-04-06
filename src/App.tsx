7850bb705003d8c6886ef063514fcc8396930ee4
yamchips <wflunch@gmail.com>
2025-04-06 15:23:13 +0100

    Build original NavBar

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `'nav' 'main'`,
+          lg: `'nav nav' 'aside main'`,
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar></NavBar>
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
+}
+
+export default App;
