ae6a1c56395131849bb7d8a0ff3f1a5ab37a9079
yamchips <wflunch@gmail.com>
2025-04-08 14:51:33 +0100

    Fetch genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import useGenres from "../hooks/useGenres";
+
+const GenreList = () => {
+  const { genres, error, isLoading } = useGenres();
+
+  return (
+    <ul>
+      {genres.map((g) => (
+        <li key={g.id}>{g.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenreList;
